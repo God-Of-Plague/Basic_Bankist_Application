d131a4eec399872d17f2ccefb6a9c751e7e2c2e0
LonelyCigarette <fftoken183@gmail.com>
2024-12-24 13:36:52 +0530

    adding data in array elements into the page

--- script.js
+++ script.js
@@ -74,3 +74,25 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+
+const displayMovements = function (movements) {
+  //to remove the previous data
+  containerMovements.innerHTML = '';
+
+  movements.forEach(function (mov, i) {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+
+    const html = `
+      <div class="movements__row">
+        <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
+        <div class="movements__value">${mov}€</div>
+      </div>
+    `;
+    //adding the data into beginnign of movements
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+
+displayMovements(account1.movements)
